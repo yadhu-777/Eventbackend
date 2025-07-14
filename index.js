6615efda0f39f0f26cc70c795db4137f924e335a
yadhu krishna <yadhukris>
2025-07-14 14:16:09 +0530

    first commit

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
     app.use(cookieParser());
 app.use(cors(
-  {origin:"http://localhost:5173",
+  {origin:"https://eventora-five.vercel.app",
     credentials:true}
 
 
